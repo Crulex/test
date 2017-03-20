d971ea6903ebcff5b0f2661148fe53e7d510788f
Crulex <mateusztracz.gda@gmail.com>
2017-03-20 12:18:23 +0100

    v 0.1.2

---
+++ mail.php
@@ -0,0 +1,9 @@
+<?php
+$to      = 'mateusztracz.gda@gmail.com';
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$headers = 'From: ' . $_POST['email'] . "\r\n" .
+    'Content-type: text/html; charset-utf-8';
+
+mail($to, $subject, $message, $headers);
+?>
